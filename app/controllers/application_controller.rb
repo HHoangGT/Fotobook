e72f7e9999f1f1b5c3bbd88865a1befaab1240b9
Nguyen Huy Hoang <hoang@internship.com>
2023-07-11 14:57:40 +0700

    Fish the edit page and add avatar, also making avatar upload in the edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fname lname])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email fname lname password avatar])
+  end
 end
