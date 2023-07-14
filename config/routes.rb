3cb2db04755035a1b3348ae92fc0b9b4ac62a76c
Nguyen Huy Hoang <hoang@internship.com>
2023-07-14 09:56:13 +0700

    add modal for profile photos page

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,12 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
+  # Photo routes
   scope :users do
     resources :photos, except: %i[index show]
   end
-
+  # Profile routes
   get '/profiles/photos', to: 'profiles#personalpagephoto'
 
   root 'main#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
