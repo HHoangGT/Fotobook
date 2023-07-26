ef6a063ef97d243347fd2222ebfc7be2d8e9d768
Nguyen Huy Hoang <hoang@internship.com>
2023-07-26 10:02:06 +0700

    fixing query for selecting photos and albums for feeds and discovers

--- app/controllers/discovers_controller.rb
+++ app/controllers/discovers_controller.rb
@@ -2,12 +2,20 @@ class DiscoversController < ApplicationController
   before_action :authenticate_user!, except: %i[album photo]
 
   def photo
-    @photos = Photo.all.page(params[:page]).per(4)
+    @photos = if user_signed_in?
+                Photo.where.not(user_id: current_user.id).page(params[:page]).per(4)
+              else
+                Photo.all.page(params[:page]).per(4)
+              end
     render 'feeds/feeds'
   end
 
   def album
-    @albums = Album.all.page(params[:page]).per(4)
+    @albums = if user_signed_in?
+                Album.where.not(user_id: current_user.id).page(params[:page]).per(4)
+              else
+                Album.all.page(params[:page]).per(4)
+              end
     render 'feeds/albums'
   end
 end
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,17 +1,13 @@
 class FeedsController < ApplicationController
   def photo
     @users = current_user.followings
-    @users.each do |user|
-      @photos = user.photos.page(params[:page]).per(4)
-      render 'feeds/feeds'
-    end
+    @photos = Photo.where(user_id: @users.pluck(:id)).page(params[:page]).per(4)
+    render 'feeds/feeds'
   end
 
   def album
     @users = current_user.followings
-    @users.each do |user|
-      @albums = user.albums.page(params[:page]).per(4)
-      render 'feeds/albums'
-    end
+    @albums = Album.where(user_id: @users.pluck(:id)).page(params[:page]).per(4)
+    render 'feeds/albums'
   end
 end
